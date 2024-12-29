2b7bc9aeac5b34e064e7f91e92e14b52c677182a
TTDD <ttdd2001@gmail.com>
2024-12-28 22:06:51 -0500

    Subprocess

--- scripts/publish_worker.py
+++ scripts/publish_worker.py
@@ -2,6 +2,7 @@
 
 import os
 import requests
+import subprocess
 
 ACCOUNT_ID = os.getenv("CLOUDFLARE_ACCOUNT_ID")
 CLOUDFLARE_API_KEY = os.getenv("CLOUDFLARE_API_TOKEN")
@@ -12,24 +13,38 @@ HEADERS = {
     "Content-Type": "application/javascript",
 }
 
-def publish_worker(script_path):
+def publish_worker():
     """Publish the worker script to Cloudflare."""
     # We need to figure out how to get the script path
-    try:
-        with open(script_path, "r") as script_file:
-            script_content = script_file.read()
+    command = [
+        "npx",
+        "wrangler",
+        "versions",
+        "upload"
+    ]
 
-        response = requests.put(BASE_URL, headers=HEADERS, data=script_content)
+    # Run the subprocess for this
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 0:
+        print(f"Succesfully published new version.")
+    else:
+        print(f"Deployment failed: {result}")
+    
+    # try:
+    #     with open(script_path, "r") as script_file:
+    #         script_content = script_file.read()
 
-        if response.status_code == 200:
-            print(f"Worker '{WORKER_NAME}' deployed successfully.")
-            return response.json()["result"]["id"]
-        print(f"Failed to deploy worker: {response.status_code} {response.text}")
-        exit(1)
+    #     response = requests.put(BASE_URL, headers=HEADERS, data=script_content)
 
-    except Exception as e:
-        print(f"Error uploading worker: {e}")
-        exit(1)
+    #     if response.status_code == 200:
+    #         print(f"Worker '{WORKER_NAME}' deployed successfully.")
+    #         return response.json()["result"]["id"]
+    #     print(f"Failed to deploy worker: {response.status_code} {response.text}")
+    #     exit(1)
+
+    # except Exception as e:
+    #     print(f"Error uploading worker: {e}")
+    #     exit(1)
 
 if __name__ == "__main__":
-    publish_worker(f"{WORKER_NAME}/src/index.mjs")
+    publish_worker()
