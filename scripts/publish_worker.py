fc9d629b7f4bb4520f759eb551f27e275cdb15eb
TTDD <ttdd2001@gmail.com>
2024-12-29 20:33:07 -0500

    Test

--- scripts/publish_worker.py
+++ scripts/publish_worker.py
@@ -20,22 +20,6 @@ def publish_worker():
     else:
         print(f"Deployment failed: {result}")
         exit(1)
-    
-    # try:
-    #     with open(script_path, "r") as script_file:
-    #         script_content = script_file.read()
-
-    #     response = requests.put(BASE_URL, headers=HEADERS, data=script_content)
-
-    #     if response.status_code == 200:
-    #         print(f"Worker '{WORKER_NAME}' deployed successfully.")
-    #         return response.json()["result"]["id"]
-    #     print(f"Failed to deploy worker: {response.status_code} {response.text}")
-    #     exit(1)
-
-    # except Exception as e:
-    #     print(f"Error uploading worker: {e}")
-    #     exit(1)
 
 if __name__ == "__main__":
     publish_worker()
