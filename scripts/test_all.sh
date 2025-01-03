88ef8476bbec3c8fc9e75f6bcd62dce12f9b475f
TTDD <ttdd2001@gmail.com>
2025-01-03 16:00:40 -0500

    Update test

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -3,9 +3,10 @@
 dirs=$(find . -maxdepth 1 -type d -name '*worker*')
 echo "Running tests for all workers: $dirs"
 
+# We willdisable watch mode
 for dir in $dirs; do
   echo "Testing worker: $dir"
   cd "$dir"
   npm install
-  npm test
+  npm test -- --run
 done
